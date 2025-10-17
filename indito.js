4a34bd24d0cf01bf78b75bd36e83f47d977fe20a
DrozsdikAdam <drozsdik17@gmail.com>
2025-10-17 13:05:03 +0200

    initial script

---
+++ indito.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const indexRoutes = require("./routes/index")
+const authRoutes = require("./routes/auth")
+const crudRoutes = require("./routes/crud")
+const uzenetRoutes = require("./routes/uzenetek")
+const indexRoutes = require("./routes/index")
+
+dotenv.config();
+const app = express();
+
+// routes
+app.use("/", indexRoutes)
+app.use("/auth", authRoutes)
+app.use("/crud", crudRoutes)
+app.use("/uzenetek", uzenetRoutes)
+
+//server indítás
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
