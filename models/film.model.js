ca87fe723b1c67d19f5f4316cf8b9d032e4502a6
DrozsdikAdam <drozsdik17@gmail.com>
2025-10-19 20:17:10 +0200

    fixing the film model

--- models/film.model.js
+++ models/film.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = (sequelize, DataTypes) => {
     const Film = sequelize.define('Film', {
         id: {
@@ -22,29 +21,4 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     return Film;
-=======
-module.exports = (sequelize, DataTypes) => {
-    const Film = sequelize.define('Film', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        cim: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        ev: {
-            type: DataTypes.INTEGER
-        },
-        hossz: {
-            type: DataTypes.INTEGER
-        }
-    }, {
-        tableName: 'film',
-        timestamps: false
-    });
-
-    return Film;
->>>>>>> 9e8e517c99b0d2aa181a8b1e18e69588fbbe019c
 };
